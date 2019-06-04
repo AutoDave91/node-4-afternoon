3a5ca249635ffb44005f4bec9ac88f61e58fae4a
david <davidwnewman91@gmail.com>
2019-06-04 15:36:05 -0500

    user login/out

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,2 +1,39 @@
-const auth = require('../models/users');
+const users = require('../models/users');
+let id = 1;
 
+function register(req, res){
+    let {session}=req;
+    let {username, password}=req.body;
+
+    users.push({id, username, password});
+    id++;
+    session.user.username =username;
+    res.status(200).send(session.user)
+}
+function login(req, res){
+    let {session}=req;
+    let {username, password}=req.body;
+
+    let user = users.find(user =>user.username === username && user.password === password);
+    if(user){
+        session.user.username = user.username;
+        res.status(200).send(session.user);
+    } else {
+        res.status(500).send('UNAUTHORIZED_USER')
+    }
+}
+function signout(req, res){
+    let {session}=req;
+
+    session.destroy();
+    res.sendStatus(200);
+}
+function getUser(req, res){
+    let {session}=req;
+
+    res.status(200).send(session.user);
+}
+
+module.exports ={
+    register, login, signout, getUser
+}
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const checkSession = require('./middlewares/checkForSession');
 const swagger = require('./controllers/swagController');
+const users = require('./controllers/authController');
 
 app.use(express.json());
 const {SERVER_PORT, SESSION_SECRET}= process.env;
@@ -16,5 +17,10 @@ app.use(session({
 app.use(checkSession)
 
 app.get('/api/swag', swagger.getSwag)
+app.get('/api/user', users.getUser)
+
+app.post('/api/login', users.login)
+app.post('/api/register', users.register)
+app.post('/api/signout', users.signout)
 
 app.listen(SERVER_PORT, ()=> console.log(`Listening on server port ${SERVER_PORT}`))
